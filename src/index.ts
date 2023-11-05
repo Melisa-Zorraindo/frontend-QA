a2d888a22b73809c99ae4ab95d782cf5537795c3
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-11-05 16:53:45 +0100

    Scaffolds project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello, world!");
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import questions from "./questions.json";
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/api/frontend-questions", (req: Request, res: Response) => {
+  res.json(questions);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
